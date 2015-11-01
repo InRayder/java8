344db731d514d67aed1720185833307a3c109c22
starhq <50849806@qq.com>
2015-11-01 14:44:02 +0800

    BASE64与单向加密算法MD5&SHA&MAC

---
+++ src/main/java/com/star/security/Coder.java
@@ -0,0 +1,111 @@
+package com.star.security;
+
+import java.security.MessageDigest;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ * 
+ * @author http://snowolf.iteye.com/blog/379860
+ *
+ */
+public class Coder {
+
+	private static final String KEY_MD5 = "MD5";
+	public static final String KEY_SHA = "SHA";
+
+	/**
+	 * MAC算法可选以下多种算法
+	 * 
+	 * <pre>
+	 * HmacMD5  
+	 * HmacSHA1  
+	 * HmacSHA256  
+	 * HmacSHA384  
+	 * HmacSHA512
+	 * </pre>
+	 */
+	public static final String KEY_MAC = "HmacMD5";
+
+	/**
+	 * 解密
+	 * 
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] decryptBASE64(String key) throws Exception {
+		// return (new BASE64Decoder()).decodeBuffer(key);
+		return Base64.decodeBase64(key);
+	}
+
+	/**
+	 * 解密
+	 * 
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static String encryptBASE64(byte[] key) throws Exception {
+		// return (new BASE64Encoder()).encodeBuffer(key);
+		return Base64.encodeBase64String(key);
+	}
+
+	/**
+	 * md5加密
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] encryptMD5(byte[] data) throws Exception {
+		MessageDigest md5 = MessageDigest.getInstance(KEY_MD5);
+		md5.update(data);
+
+		return md5.digest();
+	}
+
+	/**
+	 * sha加密
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] encryptSHA(byte[] data) throws Exception {
+		MessageDigest md5 = MessageDigest.getInstance(KEY_SHA);
+		md5.update(data);
+		return md5.digest();
+	}
+
+	/**
+	 * 初始化密钥
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static String initMacKey() throws Exception {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_MAC);
+		SecretKey secretKey = keyGenerator.generateKey();
+		return encryptBASE64(secretKey.getEncoded());
+	}
+
+	/**
+	 * mac加密
+	 */
+	public static byte[] encryptHMAC(byte[] data, String key) throws Exception {
+		SecretKey secretKey = new SecretKeySpec(decryptBASE64(key), KEY_MAC);
+		Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+		mac.init(secretKey);
+
+		return mac.doFinal(data);
+	}
+}
--- src/main/java/com/star/security/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.star.security;
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-
-}
